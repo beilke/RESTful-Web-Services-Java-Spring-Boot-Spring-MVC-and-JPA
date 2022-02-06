609ed982e7e86b223b2017a0df1961374d4b7b58
fernando.beilke <fernando.beilke@localhost>
2022-02-06 19:25:21 +0100

    Initial implementation of the Spring Security

---
+++ mobile-app-ws/src/main/java/com/appsdeveloper/app/ws/security/SecurityConstants.java
@@ -0,0 +1,12 @@
+package com.appsdeveloper.app.ws.security;
+
+public class SecurityConstants {
+	public static final String SIGN_UP_URL = "/users";
+
+	private SecurityConstants() { } // Prevents instantiation
+	
+	
+	
+	
+
+}
